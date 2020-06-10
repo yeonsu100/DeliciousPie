5c3e2ed1ed1489c4c99fc99680e9b53a11678a61
yeonsu100 <yeonsu100@gmail.com>
2020-06-10 19:18:30 +0900

    list practice 16

---
+++ Practice/list16c.py
@@ -0,0 +1,7 @@
+# list of numbers
+list1 = [10, 21, 4, 45, 66, 93]
+
+# using list comprehension
+even_nos = [num for num in list1 if num % 2 == 0]
+
+print("Even numbers in the list: ", even_nos)
---
+++ Practice/list16d.py
@@ -0,0 +1,7 @@
+# list of numbers
+list1 = [10, 21, 4, 45, 66, 93, 11]
+
+# we can also print even no's using lambda exp.
+even_nos = list(filter(lambda x: (x % 2 == 0), list1))
+
+print("Even numbers in the list: ", even_nos) 
