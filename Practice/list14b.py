02389ca196dbef5ddc98e0ebd9e791b36feb8cd9
yeonsu100 <yeonsu100@gmail.com>
2020-06-10 19:15:52 +0900

    list practice 14

---
+++ Practice/list14b.py
@@ -0,0 +1,8 @@
+# list of numbers
+list1 = [10, 20, 4, 45, 99]
+
+# sorting the list
+list1.sort()
+
+# printing the second last element
+print("Second largest element is:", list1[-2])
---
+++ Practice/list14c.py
@@ -0,0 +1,13 @@
+# list of numbers
+list1 = [10, 20, 4, 45, 99]
+
+# new_list is a set of list1
+new_list = set(list1)
+
+# removing the largest element from temp list
+new_list.remove(max(new_list))
+
+# elements in original list are not changed
+# print(list1)
+
+print(max(new_list)) 
