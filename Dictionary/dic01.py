6e7d8e6a3891a61d87be5a38a309916b7a91111b
yeonsu100 <yeonsu100@gmail.com>
2020-06-16 22:47:53 +0900

    dictionary practice 1

---
+++ Dictionary/dic01.py
@@ -0,0 +1,16 @@
+import random as rn
+
+# creating an empty dictionary
+dict = {}
+
+# Insert first triplet in dictionary
+x, y, z = 10, 20, 30
+dict[x, y, z] = x + y - z;
+
+# Insert second triplet in dictionary
+x, y, z = 5, 2, 4
+dict[x, y, z] = x + y - z;
+
+# print the dictionary
+print(dict)
+
