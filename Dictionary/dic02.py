4e87c53c6e58c578b90cb7edc009fcec541b12d3
yeonsu100 <yeonsu100@gmail.com>
2020-06-16 22:49:48 +0900

    dictionary practice 2

---
+++ Dictionary/dic02.py
@@ -0,0 +1,12 @@
+# Function to print sum
+def returnSum(myDict):
+    sum = 0
+    for i in myDict:
+        sum = sum + myDict[i]
+
+    return sum
+
+
+# Driver Function
+dict = {'a': 100, 'b': 200, 'c': 300}
+print("Sum :", returnSum(dict))
