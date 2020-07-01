258327070b0443ad2a21e009aeee088ff3d2c06b
yeonsu100 <yeonsu100@gmail.com>
2020-07-01 20:06:27 +0900

    Find largest prime factor of a number

---
+++ Exercise/Ex02.py
@@ -0,0 +1,29 @@
+import math
+
+
+# A function to find largest prime factor
+def maxPrimeFactors(n):
+    maxPrime = -1
+
+    # Print the number of 2s that divide n
+    while n % 2 == 0:
+        maxPrime = 2
+        n >>= 1  # equivalent to n /= 2
+
+    for i in range(3, int(math.sqrt(n)) + 1, 2):
+        while n % i == 0:
+            maxPrime = i
+            n = n / i
+
+    if n > 2:
+        maxPrime = n
+
+    return int(maxPrime)
+
+
+# Driver code to test above function
+n = 15
+print(maxPrimeFactors(n))
+
+n = 25698751364526
+print(maxPrimeFactors(n))
