e8185d263eb212a890cdbb2b9ee2a81096c3138a
yeonsu100 <yeonsu100@gmail.com>
2020-09-21 23:15:10 +0900

    Matrix Kth column summation using loop

---
+++ Exercise/Ex50_03.py
@@ -0,0 +1,21 @@
+# initialize list
+test_list = [[5, 6, 7],
+             [9, 10, 2],
+             [10, 3, 4]]
+
+# printing original list
+print("The original list is : " + str(test_list))
+
+# initialize K
+K = 2
+
+# Matrix Kth column summation
+# Using loop
+res = 0
+for sub in test_list:
+    for idx in range(0, len(sub)):
+        if idx == K:
+            res = res + sub[idx]
+
+        # printing result
+print("Sum of Kth column is : " + str(res))
