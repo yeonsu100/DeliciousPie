b4b6f9da26f4ff93760d00089cfeac170005bce8
yeonsu100 <yeonsu100@gmail.com>
2020-09-22 21:44:38 +0900

    Shrink gap by Shrink factor

---
+++ Exercise/Ex54.py
@@ -0,0 +1,30 @@
+# To find next gap from current
+def getNextGap(gap):
+    # Shrink gap by Shrink factor
+    gap = (gap * 10) / 13
+    if gap < 1:
+        return 1
+    return gap
+
+
+# Function to sort arr[] using Comb Sort
+def combSort(arr):
+    n = len(arr)
+
+    # Initialize gap
+    gap = n
+
+    swapped = True
+
+    while gap != 1 or swapped == 1:
+
+        # Find next gap
+        gap = getNextGap(gap)
+
+        swapped = False
+
+        # Compare all elements with current gap
+        for i in range(0, n - gap):
+            if arr[i] > arr[i + gap]:
+                arr[i], arr[i + gap] = arr[i + gap], arr[i]
+                swapped = True
